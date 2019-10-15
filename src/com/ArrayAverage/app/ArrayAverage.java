4718dfb4172f9ffba6292dd84c707b68a0f19906
Mabestron <ivangamaliel555@hotmail.com>
2019-10-14 23:46:59 -0500

    Archivo terminado del promedio

--- src/com/ArrayAverage/app/ArrayAverage.java
+++ src/com/ArrayAverage/app/ArrayAverage.java
@@ -1,10 +1,35 @@
 package com.ArrayAverage.app;
+import java.util.Random;
+public class ArrayAverage 
+{
 
-public class ArrayAverage {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+				//Constant
+				final int ELEMENTS_ARRAY=5;
+				
+				//Variable
+				int average=0;
+				
+				//Array Declaration
+				byte[] vector = new byte [ELEMENTS_ARRAY];
+				
+				//Object declaration
+				Random randomNumbers = new Random(System.nanoTime());
+				
+				//Relleno del vector con numeros aleatorios
+				for (int i=0;i<ELEMENTS_ARRAY;i++)
+				{
+					vector[i]= (byte) randomNumbers.nextInt(10);
+					System.out.println(vector[i]);
+					average += vector[i];
+				}
+				
+				
+				average= average/ELEMENTS_ARRAY;
+				
+				System.out.println("El Promedio es: "+average);
+				
 	}
 
 }
